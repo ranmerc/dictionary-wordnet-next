1f32967876af4dab6274066f188044d19207b502
Kamran Ansari <crossthinking786@gmail.com>
2021-11-17 23:07:21 +0530

    recent page fe completed

---
+++ pages/recent.js
@@ -0,0 +1,34 @@
+import Head from 'next/head';
+import ListHeader from '../components/ListHeader';
+import SearchList from '../components/SearchList';
+import SearchListItem from '../components/SearchListItem';
+
+export default function Recent() {
+  return (
+    <>
+      <Head>
+        <title>Recent</title>
+        <meta name="description" content="Recent page for the dictionary app" />
+      </Head>
+      <main>
+        <ListHeader enable={false}>Recent</ListHeader>
+        <SearchList>
+          <SearchListItem />
+          <SearchListItem />
+          <SearchListItem />
+          <SearchListItem />
+          <SearchListItem />
+          <SearchListItem />
+          <SearchListItem />
+        </SearchList>
+        {/* <ListMessage>List empty. Recent words will show up here.</ListMessage> */}
+      </main>
+      <style jsx>{`
+        main {
+          display: grid;
+          row-gap: 1.56rem;
+        }
+      `}</style>
+    </>
+  );
+}
