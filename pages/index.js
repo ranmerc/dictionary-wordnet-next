528b2227dc8ace55484d552045d831879c66a167
Kamran Ansari <crossthinking786@gmail.com>
2021-11-21 21:02:27 +0530

    added redirect in index page

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,12 @@
-import { useThemeContext } from '../context/ThemeContext';
+import { useRouter } from 'next/dist/client/router';
+import { useLayoutEffect } from 'react';
 
 export default function Home() {
-  const { theme, setTheme } = useThemeContext();
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <button
-        onClick={() => {
-          setTheme({
-            type: 'TOGGLE_MODE',
-          });
-        }}
-      >
-        Toggle Theme
-      </button>
-      <style jsx>{`
-        h1 {
-          color: ${theme.color[10]};
-        }
-      `}</style>
-    </div>
-  );
+  const router = useRouter();
+
+  useLayoutEffect(() => {
+    router.replace('/search');
+  }, []);
+
+  return null;
 }
