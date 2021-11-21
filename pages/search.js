5c3343fed02f391f1e6bd6b06ba82c3bce46c63f
Kamran Ansari <crossthinking786@gmail.com>
2021-11-21 22:53:59 +0530

    search page fe completed

--- pages/search.js
+++ pages/search.js
@@ -1,3 +1,44 @@
+import Head from 'next/head';
+import { useState } from 'react';
+import SearchBar from '../components/SearchBar';
+import SearchList from '../components/SearchList';
+import SearchListItem from '../components/SearchListItem';
+
 export default function search() {
-  return <div>Hello</div>;
+  const [query, setQuery] = useState('bow');
+
+  return (
+    <>
+      <Head>
+        <title>Search</title>
+        <meta name="description" content="Search page for the dictionary app" />
+      </Head>
+      <main>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <SearchBar query={query} setQuery={setQuery} />
+        </form>
+        <section>
+          <SearchList>
+            <SearchListItem />
+            <SearchListItem />
+            <SearchListItem />
+            <SearchListItem />
+            <SearchListItem />
+            <SearchListItem />
+            <SearchListItem />
+          </SearchList>
+        </section>
+        <style jsx>{`
+          main {
+            display: grid;
+            row-gap: 2.2rem;
+          }
+        `}</style>
+      </main>
+    </>
+  );
 }
