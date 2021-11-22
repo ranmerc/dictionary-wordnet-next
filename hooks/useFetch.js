f12e7498a01f9ae1c33bee7f1dbd88192f43e0d5
Kamran Ansari <crossthinking786@gmail.com>
2021-11-22 13:11:27 +0530

    add useFetch hook

---
+++ hooks/useFetch.js
@@ -0,0 +1,22 @@
+import useSWR from 'swr';
+
+const fetcher = async (url) => {
+  console.log('fetching', url);
+  const res = await fetch(url);
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error);
+  }
+  return res.json();
+};
+
+export default function useFetch(url) {
+  const { data, error } = useSWR(url, fetcher, {
+    revalidateOnFocus: false,
+  });
+  return {
+    data: data,
+    isLoading: !data && !error,
+    isError: error,
+  };
+}
