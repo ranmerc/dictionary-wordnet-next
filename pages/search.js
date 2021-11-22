4627691f79d9d6dc64945c7788a47b0ac0c1b5a5
Kamran Ansari <crossthinking786@gmail.com>
2021-11-22 13:24:02 +0530

    added search functionality to search page

--- pages/search.js
+++ pages/search.js
@@ -1,11 +1,18 @@
 import Head from 'next/head';
 import { useState } from 'react';
+import useFetch from '../hooks/useFetch';
 import SearchBar from '../components/SearchBar';
 import SearchList from '../components/SearchList';
+import ListMessage from '../components/ListMessage';
 import SearchListItem from '../components/SearchListItem';
 
 export default function search() {
-  const [query, setQuery] = useState('bow');
+  const [query, setQuery] = useState('');
+  // dont fetch if query is empty
+  // https://swr.vercel.app/docs/conditional-fetching#conditional
+  const { data, isLoading, isError } = useFetch(
+    query ? `/api/querysense?word=${query}` : null
+  );
 
   return (
     <>
@@ -22,14 +29,29 @@ export default function search() {
           <SearchBar query={query} setQuery={setQuery} />
         </form>
         <section>
+          {/* no input, show encouraging message */}
+          {!query && (
+            <ListMessage>
+              Type your word above. Results will show up here!
+            </ListMessage>
+          )}
+          {/* input and loading */}
+          {query && isLoading && <ListMessage>Loading...</ListMessage>}
+          {/* no result found */}
+          {Array.isArray(data) && !data.length && (
+            <ListMessage>No word found. Try to be specific</ListMessage>
+          )}
+          {/* fails eg. no internet connection */}
+          {isError && (
+            <ListMessage>
+              Something went wrong! Check your internet connection!
+            </ListMessage>
+          )}
           <SearchList>
-            <SearchListItem />
-            <SearchListItem />
-            <SearchListItem />
-            <SearchListItem />
-            <SearchListItem />
-            <SearchListItem />
-            <SearchListItem />
+            {data &&
+              data.map((meaning) => {
+                return <SearchListItem {...meaning} key={meaning.sense} />;
+              })}
           </SearchList>
         </section>
         <style jsx>{`
