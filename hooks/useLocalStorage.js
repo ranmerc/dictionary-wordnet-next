efa793ba7ca421f4f4004d76fbe18e49a8efd4d5
Kamran Ansari <crossthinking786@gmail.com>
2021-11-25 11:12:46 +0530

    refactor hooks/useLocalStorage

--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -1,4 +1,4 @@
-import { useReducer, useEffect, useLayoutEffect, useMemo } from 'react';
+import { useReducer, useEffect, useLayoutEffect, useMemo, useRef } from 'react';
 
 // checks if the str is json
 const isJSON = (str) => {
@@ -16,22 +16,40 @@ export default function useLocalStorage(key, initialState, reducer) {
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  // for stop running effect on first render
+  // https://stackoverflow.com/questions/53253940/make-react-useeffect-hook-not-run-on-initial-render
+  const firstTime = useRef(true);
+
   // as useLayoutEffect runs before screen is visually updated we
   // use it instead of useEffect here to hide the flash of intialState theme
   // initially after mounting if a theme exists in localstorage, apply it
   useLayoutEffect(() => {
-    // because if key doesn't exists getItems returns null
-    if (localStorage.getItem(key) && isJSON(localStorage.getItem(key))) {
+    // get the item stored with the key from the localstorage
+    const objectFromLocalStorage = localStorage.getItem(key);
+    // checking for falseness because if key doesn't exists getItems returns null
+    if (
+      objectFromLocalStorage &&
+      isJSON(objectFromLocalStorage) &&
+      // do not update state if locally stored object is same as state
+      objectFromLocalStorage !== JSON.stringify(initialState)
+    ) {
       dispatch({
         type: 'init_stored',
-        value: JSON.parse(localStorage.getItem(key)),
+        value: JSON.parse(objectFromLocalStorage),
       });
     }
   }, []);
 
   // subsequently if the state changes, store the changes to localStorage
   useEffect(() => {
-    if (state && state !== initialState) {
+    // don't do anything on first render
+    if (firstTime.current) {
+      firstTime.current = false;
+      return;
+    }
+    // before writing to local storage check if stored object is same as state
+    // prevents unnecessary writes to localStorage
+    if (window && JSON.stringify(window.localStorage.getItem(key)) !== state) {
       localStorage.setItem(key, JSON.stringify(state));
     }
   }, [state]);
