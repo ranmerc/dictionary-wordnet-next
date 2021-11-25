30fc6815315346a8abb049cd761ccaa2b8633e2d
Kamran Ansari <crossthinking786@gmail.com>
2021-11-25 14:20:20 +0530

    create RecentContext

---
+++ context/RecentContext.js
@@ -0,0 +1,49 @@
+import { createContext, useContext } from 'react';
+import useLocalStorage from '../hooks/useLocalStorage';
+
+const RecentContext = createContext();
+
+const RECENT_SIZE = 50;
+
+// state is previous state, action is object passed to dispatch
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'init_stored':
+      return action.value;
+    case 'ADD':
+      return [action.value].concat(
+        state
+          .filter((sense) => {
+            return sense.offset !== action.value.offset;
+          })
+          .slice(0, RECENT_SIZE - 1)
+      );
+    case 'CLEAR':
+      return [];
+  }
+};
+
+function RecentProvider({ children }) {
+  // rename state and dispatch properties
+  const { state: recent, dispatch: setRecent } = useLocalStorage(
+    'recent',
+    [],
+    reducer
+  );
+  return (
+    <RecentContext.Provider value={{ recent, setRecent }}>
+      {children}
+    </RecentContext.Provider>
+  );
+}
+
+// return a wrapped hook to use in components
+function useRecentContext() {
+  const context = useContext(RecentContext);
+  if (context === undefined) {
+    throw new Error('useRecentContext must be used within a RecentProvider');
+  }
+  return context;
+}
+
+export { RecentProvider, useRecentContext };
--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import Layout from '../components/Layout';
 import { IdProvider } from '@radix-ui/react-id';
 import { ThemeProvider } from '../context/ThemeContext';
 import { QueryProvider } from '../context/QueryContext';
+import { RecentProvider } from '../context/RecentContext';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -12,17 +13,19 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      {/* from radixui for consistent ssr */}
+      {/* from radix-ui for consistent ssr */}
       <IdProvider>
         <ThemeProvider>
           {/* persistent query input between page navigation */}
           <QueryProvider>
-            {/* custom layout skeleton for the app */}
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-            {/* nav bar present on every page */}
-            <NavBar />
+            <RecentProvider>
+              {/* custom layout skeleton for the app */}
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+              {/* nav bar present on every page */}
+              <NavBar />
+            </RecentProvider>
           </QueryProvider>
         </ThemeProvider>
       </IdProvider>
