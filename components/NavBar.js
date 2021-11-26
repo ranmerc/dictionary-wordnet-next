4dc6cab058587a814df1c6a5f650989e9bb70863
Kamran Ansari <crossthinking786@gmail.com>
2021-11-26 13:48:18 +0530

    change back to search in NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -82,7 +82,7 @@ const svgs = {
       viewBox="0 0 15 15"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      aria-label="Go back"
+      aria-label="Go back to search page"
     >
       <path
         d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 11.3417 7.04882 11.6583 6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645L6.14645 3.14645C6.34171 2.95118 6.65829 2.95118 6.85355 3.14645Z"
@@ -115,7 +115,7 @@ export default function NavBar() {
         {/* return different navbar if on /define */}
         {ifDefine && (
           <ul>
-            <NavBarItem route="Back" href="/search" svg={svgs['back']} />
+            <NavBarItem route="Search" href="/search" svg={svgs['back']} />
             <BookmarkCheckBox />
           </ul>
         )}
